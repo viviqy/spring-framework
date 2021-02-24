1272ce995512c3075485e96fe1d143abf86c6730
chiyo <1261975105@qq.com>
2021-02-24 22:28:28 +0800

    add ProxySample

--- core-learning/src/test/java/ProxySample.java
+++ core-learning/src/test/java/ProxySample.java
@@ -1,17 +1,47 @@
-package com.fairychar.core.learning.configuration;
+import org.springframework.context.annotation.Scope;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 /**
- * Datetime: 2020/12/22 15:23 <br>
+ * Created with IDEA <br>
+ * Date: 2021/02/24 <br>
+ * time: 21:54 <br>
  *
  * @author chiyo <br>
- * @since 1.0
  */
-@ComponentScan(basePackages = "com.fairychar")
-@EnableAspectJAutoProxy()
-public class AppConfiguration {
+
+public class ProxySample {
+
+	public static void main(String[] args) {
+		UserDao mapper = UserFactory.getMapper(UserDao.class);
+		System.out.println(mapper.findAll());
+	}
+
+
+	interface UserDao {
+		@Scope("select * from user")
+		String findAll();
+
+	}
+
+	static class UserFactory {
+		static <T> T getMapper(Class<T> clazz) {
+			Object o = Proxy.newProxyInstance(UserFactory.class.getClassLoader(), new Class[]{clazz}, new UserInvocationHandler());
+			return (T)o;
+		}
+	}
+
+	static class UserInvocationHandler implements InvocationHandler {
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			System.out.println("get sql session");
+			Scope scope = method.getAnnotation(Scope.class);
+			System.out.println("select from db sql=" + scope.value());
+			return "all users";
+		}
+	}
 }
 /*
                                       /[-])//  ___        
